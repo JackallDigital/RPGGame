13798d9f0ad131047e654b1f32ebd7490a1c531f
Din Dobrnjic <din.dobrnjic@ineor.si>
2023-07-03 13:53:43 +0200

    Fixed the camera rotation and weapon animation

--- Assets/Scripts/PlayerScripts/RotateWeaponToMouse.cs
+++ Assets/Scripts/PlayerScripts/RotateWeaponToMouse.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class RotateWeaponToMouse : MonoBehaviour
 {
     public Transform player; // Assign this to your player in the Inspector
-     public float radius = 1.0f; // Distance from the player
+    public float radius = 1.0f; // Distance from the player
     public float minMouseDistance = 0.5f; // Minimum distance mouse has to be from player
-
+    private bool isFlipped = false; // track if the sprite is flipped
 
     void Update()
     {
@@ -24,10 +24,18 @@ public class RotateWeaponToMouse : MonoBehaviour
             // Rotate weapon to face mouse
             direction = mousePos - transform.position;
             angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            if(isFlipped)
+            {
+                // if the sprite is flipped, we need to invert the angle
+                angle = -angle;
+            }
             transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
-
-          
-         
         }
+    }
+
+    // Call this function when you flip the sprite
+    public void FlipSprite()
+    {
+        isFlipped = !isFlipped;
     }
 }
